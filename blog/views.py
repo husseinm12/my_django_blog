b5496dd412d8dddd2ee693766533ff9b37237ba8
Hussein Ali Moussa <hussein.moussa2004@gmail.com>
2025-08-24 23:12:00 +0300

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+
+
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'posts': posts})
+
+# View post details
+@login_required
+def post_details(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+#Create a new post
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+    success_url = reverse_lazy('home')
+
+# Update an existing post
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+    success_url = reverse_lazy('home')
+
+# Delete a post
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url =reverse_lazy('home')
+
